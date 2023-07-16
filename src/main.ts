74905963bd176ffad4de54aac45458f003cec3e6
adrianBrands <40333282+adrianBrands@users.noreply.github.com>
2023-07-16 08:40:06 +0200

    testing out typescript in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,11 @@
-import './style.css'
+import "./style.css";
 
+const getWelcomeClass: HTMLElement | null = document.querySelector(".welcome");
+
+const createH1: HTMLHeadingElement = document.createElement("h1");
+
+const greeting: string = "Velkommen til Bilskilt Scanner";
+
+createH1.append(greeting);
+
+getWelcomeClass?.append(createH1);
